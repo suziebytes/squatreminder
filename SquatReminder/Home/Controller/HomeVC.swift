2b10dbdb270025dd6dd1da4e05e904bc2f497841
Suzie <reorsusie@gmail.com>
2023-03-03 10:47:01 -0800

    Added app icon

--- SquatReminder/Home/Controller/HomeVC.swift
+++ SquatReminder/Home/Controller/HomeVC.swift
@@ -41,11 +41,8 @@ class HomeVC: UIViewController, NotificationViewDelegate {
         } else {
             notificationView.onOffSwitch.setOn(false, animated: false)
         }
-        
+ 
         NotificationCenter.default.addObserver(self, selector: #selector(willEnterForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
-
-//        let squatCount = UserDefaults.standard.integer(forKey: "logSquats")
-//        todayView.currentSquatButton.setTitle(String(squatCount), for: .normal)
     }
     
     
